b53519b424a09bf3e7d6b6d1b6a37addaea7e6e9
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-03 03:51:18 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,14 +1,15 @@
-
+require 'pry'
 
 class Course
   attr_accessor :title, :schedule, :description
   @@all = []
 
   def initialize
-    @@all << self  
-  end  
-  
+    @@all << self
+  end
+
   def self.all
     @@all
-  end 
+
+  end
 end
